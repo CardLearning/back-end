f584ad84c75586c4b621c18995f82411b455b9c8
Sergey Dubovich <losclipp3@gmail.com>
2025-11-03 17:50:06 +0100

    Added Swagger

--- CardLearningAPI/CardLearningAPI/Program.cs
+++ CardLearningAPI/CardLearningAPI/Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo{ Title = "CardLearningAPI", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -19,5 +26,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 app.Run();
